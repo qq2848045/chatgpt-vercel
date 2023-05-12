50aeff637922f2b9e75b20cea67107a1244323ff
qq2848045 <60593825+qq2848045@users.noreply.github.com>
2023-05-12 22:05:39 +0800

    Update index.ts

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -88,7 +88,7 @@ export async function POST({ request }: APIEvent) {
 
     const apiKey = randomKey(splitKeys(key))
 
-    if (!apiKey) throw new Error("没有填写 OpenAI API key，或者 key 填写错误。点击拥有自己的OpenAI账户.[点击获取自己的OpenAI账户](https://pp0x0qq.com)")
+    if (!apiKey) throw new Error("没有填写 OpenAI API key，或者 key 填写错误。[点击获取自己的OpenAi账户](https://pp0x0qq.com)")
 
     const encoder = new TextEncoder()
     const decoder = new TextDecoder()
